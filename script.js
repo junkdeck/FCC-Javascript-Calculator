6233b24c0bf07826375394e2c36dde93a0e306ad
junkdeck <fred@mslsr.com>
2017-04-16 23:50:54 +0200

    operate on result
    can now operate on the result. also returns an error on division with 0.
    just need to calculate the result when stringing operators together and
    i am done!!

--- script.js
+++ script.js
@@ -29,29 +29,7 @@ function inputSwitch(input){
             operationArray.push(inputArray.join(''));
             //convert to function later, maybe?
             //goes through each entry in operationArray, two at a time, and stores it in a result variable that is used against the next expression. no order of execution here, folks!
-            let a = 0;
-            let b = 0;
-            let op = '';
-            operationArray.forEach(function(x,i){
-                console.log("a: "+a);
-                console.log("b: "+b);
-                //even index are nums, odd index are operators
-                if(i%2!==0){    //even index, op
-                    op = x;
-                }else if(i%2===0 && !a && !b){   //uneven index & unset a, num
-                    console.log(i);
-                    a = x;
-                }else if(i%2===0 && !b){
-                    b = x;
-                }
-                if(a && b){
-                    let result = doMath[op](a,b);
-                    clearDisplay();
-                    pushToDisplay(result)
-                    isEvalDone = true;
-                }
-            });
-
+            performOps();
             operationArray = [];        //empty operations array so continuous evaluation doesn't just compound the current integers
             console.log("equal");
             return false;
@@ -67,22 +45,29 @@ function inputSwitch(input){
             }
             break;
         default:
+            if(inputArray[0]==0){
+                inputArray.splice(0,1);
+            }
             //if an operand has been chosen, empty the display when entering the next "step" of the calculation
             if(isOperandChosen){
                 operationArray.push(inputArray.join(''), chosenOperand);
                 clearDisplay();
-            }
-            if(isEvalDone){
+            }else if(isEvalDone&&!isOperandChosen){
                 isEvalDone = false;
                 clearDisplay();
             }
+            // else if(isEvalDone&&isOperandChosen){
+            //     operationArray.push(inputArray.join(''), chosenOperand);
+            //     clearDisplay();
+            // }
             //removes leading zero from cleared display
-            if(displayArray[0] == 0){
+            if(displayArray[0] == 0 && displayArray.length === 1){
                 displayArray.splice(0,1);
             }
             //pushes integers to both input and display arrays
             isOperandChosen = false;
             pushToInputDisplay(input);
+            console.log("input: "+inputArray);
             console.log("num");
             return true;
             break;
@@ -119,7 +104,9 @@ function hasDecimalDot(input){
 }
 //checks the current number of display characters - deletes the first index of display numbers if numbers exceed display character limit
 function checkDisplayCharacterNum(){
-    currentNumbersOnDisplay++;
+    if(inputArray[0] != 0){
+        currentNumbersOnDisplay++;
+    }
     if(currentNumbersOnDisplay > 11){
         displayArray.splice(0,1);
         currentNumbersOnDisplay=11;
@@ -132,13 +119,44 @@ function clearDisplay(){
     displayArray = [0];
     emptyDisplay();
 }
+//performs the operations on the input numbers
+function performOps(){
+    console.log("input: "+inputArray);
+    console.log("display: "+displayArray);
+    console.log("operation: "+operationArray);
+    let a = 0;
+    let b = 0;
+    let op = '';
+    operationArray.forEach(function(x,i){
+        //even index are nums, odd index are operators
+        if(i%2!==0){    //even index, op
+            op = x;
+        }else if(i%2===0 && !a && !b){   //uneven index & unset a, num
+            a = x;
+        }else if(i%2===0 && !b){
+            b = x;
+        }
+        if(a && b){
+            console.log("a: "+a);
+            console.log("b: "+b);
+            let result = doMath[op](a,b);
+            clearDisplay();
+            pushToInputDisplay(result);
+            isEvalDone = true;
+        }
+    });
+}
 
 //math function - object containing multiple arithmetic functions
 //doMath[operand](x,y);
 let doMath = {
     '+':function(x,y){return parseFloat(x)+parseFloat(y);},
     '-':function(x,y){return parseFloat(x)-parseFloat(y);},
-    '/':function(x,y){return parseFloat(x)/parseFloat(y);},
+    '/':function(x,y){
+        if(x==0||y==0){
+            return "DIVISION BY 0";
+        }
+        return parseFloat(x)/parseFloat(y);},
     'x':function(x,y){return parseFloat(x)*parseFloat(y);},
 }
 
@@ -174,6 +192,7 @@ $('.button').on('mousedown',function(){
 
 $('.clear').on('mousedown',function(){
     //clears both input and display array as well as flushes display
+    operationArray = [];
     clearDisplay();
     appendToDisplay(displayArray);
 });
